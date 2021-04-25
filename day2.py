82f58a33ed98888b97f040b1c20e84799a2265e9
Pranalihalageri <54852298+Pranalihalageri@users.noreply.github.com>
2021-04-25 16:28:36 +0530

    Create day2.py

---
+++ day2.py
@@ -0,0 +1,16 @@
+
+
+1.
+Name = "SRK"
+b_yrs = 25
+box_off_col = 12345.12
+Married = True
+
+2.
+print("Hi {} is my favourite hero, has completed {} years in bollywood with all superhit movies collection  {}".format(Name,b_yrs,box_off_col))
+
+3.
+Real life example of variable
+ 
+Everyone has a saving / current bank account. From this account you can withdraw money from ATM or deposit money by visiting the bank.
+
