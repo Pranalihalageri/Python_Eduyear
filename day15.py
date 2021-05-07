b0eae370bc8b5dc26342acbf5988b1a3b813b736
Pranalihalageri <54852298+Pranalihalageri@users.noreply.github.com>
2021-05-07 19:29:26 +0530

    Create day15.py

---
+++ day15.py
@@ -0,0 +1,15 @@
+1.
+import numpy as np
+print(np.__version__)
+
+2.
+import numpy as np
+my_list = [[12.23,13.32,100,36.32]]
+my_array = np.asarray(my_list)
+# printing my_array
+print (my_array)
+
+3.
+import numpy as np
+x =  np.arange(2, 11).reshape(3,3)
+print(x)
